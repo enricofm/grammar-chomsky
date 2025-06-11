1114788e4d2e2e7dc70fa495b9695cead777bd3e
Lorenzo Venturini <102552226+LorenzoVA@users.noreply.github.com>
2025-06-10 21:23:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from grammar_functions.grammar_parser import parse_grammar
 from grammar_functions.grammar_cleaner import limpar_gramatica
-from pprint import pprint
 
 
 def main():
     grammar = parse_grammar("input.txt")
     clean_grammar = limpar_gramatica(grammar)
-    pprint(clean_grammar)
+    
+    producoes = clean_grammar.get("producoes", {})
+    
+    with open("output.txt", "w", encoding="utf-8") as f:
+        for variavel, regras in producoes.items():
+            for regra in regras:
+                linha = f"{variavel} " + " ".join(regra).replace("''", 'h')  # substitui ε por h
+                f.write(linha + "\n")
 
 
 if __name__ == "__main__":
